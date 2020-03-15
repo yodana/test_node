9418d2137b654b0d68820400dfb01d6005c81f44
Yohann Dana <yohanndana@iMac-de-Yohann.local>
2020-03-15 17:32:30 +0100

    finish to do list

--- test.js
+++ test.js
@@ -3,17 +3,33 @@ var cookie = require('cookie-session');
 var bodyParser = require('body-parser');
 
 var app = express();
-var listes = ["lol", "lol"];
+
+app.use(cookie({
+    secret: 'todotopsecret'}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next){
+    if (typeof(req.session.listes) == 'undefined') {
+        req.session.listes = [];
+    }
+    next();
+})
+
 app.get('/', function(req, res){
-    res.render('test.ejs', {liste:listes});
+    res.render('test.ejs', {liste:req.session.listes});
 });
 
-app.post('/', function(req, res){
-    //listes.push(req.body.listes);
+app.get('/supprimer/:id', function(req, res){
+    var id = req.params.id;
+    req.session.listes.splice(id, 1);
+    res.render('test.ejs', {liste:req.session.listes});
+});
+
+app.post('/ajouter', function(req, res){
+    req.session.listes.push(req.body.listes);
+    res.render('test.ejs', {liste:req.session.listes});
 });
 
 app.listen(8080);
